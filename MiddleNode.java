d1b3d357e5948dde2d89bdcc61ee3016d718848c
MANMATHA SIVA RAJAN J <sivaraj050406@gmail.com>
2026-01-29 09:58:40 +0530

    Add solution for Middle of the Linked List
    Uses slow and fast pointers to find the middle node in one pass.

---
+++ MiddleNode.java
@@ -0,0 +1,20 @@
+ // Middle of the Linked List
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+class  MiddleNode{
+    public ListNode middleNode(ListNode head) {
+        if(head == null || head.next == null)   return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        //ListNode dummy = new ListNode(0);
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
