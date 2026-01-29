c630b7e68ebe972b60c1f948aead186dde935606
MANMATHA SIVA RAJAN J <sivaraj050406@gmail.com>
2026-01-29 12:17:27 +0530

    Implement MostWordsFound class for sentence analysis
    Added a class to find the maximum number of words in sentences.

---
+++ MaximumWordsFoundInSentences.java
@@ -0,0 +1,17 @@
+// Maximum Number of Words Found in Sentences
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+// You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+// Return the maximum number of words that appear in a single sentence.
+
+class MostWordsFound {
+    public int mostWordsFound(String[] sentences) {
+        int count = 0;
+        for(String s : sentences){
+            String[] arr = s.split(" ");
+            count = Math.max(count, arr.length);
+        }
+        return count;
+    }
+}
