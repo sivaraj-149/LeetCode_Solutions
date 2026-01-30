c613df210e388403ab697b6766e5d922d38c78e6
MANMATHA SIVA RAJAN J <sivaraj050406@gmail.com>
2026-01-30 10:07:20 +0530

    Implement addition of two numbers using linked lists
    - Implemented logic to add two numbers digit by digit
    - Handled carry during addition
    - Returned result as a linked list

---
+++ AddTwoNumbers.java
@@ -0,0 +1,27 @@
+// Add Two Numbers
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0){
+            int sum = carry;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            carry = sum/10;
+        }
+        return dummy.next;
+    }
+}
